c1654478f8d8254363e4bb010e7022a33a0e72d6
Polina Deniskina <polinaoioi67@gmail.com>
2022-09-01 17:14:43 +0300

    Task29

---
+++ Seminar_4/Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из N элементов,
+// заполненных случайнми числами из [a, b) и выводит их на экран.
+//5, 0, 20 -> [1, 2, 5, 7, 19]
+//3, 1, 35 -> [6, 1, 33]
+
+int[] FillArray(int N, int A, int B)
+{ 
+  int[] array = new int[N];
+  var random = new Random();
+  for (int i = 0; i < N; i++)
+  {
+    array[i] = random.Next(A, B);
+  }
+  return array;
+}
+Console.Write("Введите число N: ");
+int numN = int.Parse(Console.ReadLine());
+Console.Write("Введите число A: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = int.Parse(Console.ReadLine());
+
+int[] arr = FillArray(numN, numA, numB);
+Console.WriteLine($"[{String.Join(",", arr)}]");
