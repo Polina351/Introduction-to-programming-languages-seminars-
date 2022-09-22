fc1562dafa9608c073bef2e0883582a3ade4e334
Polina Deniskina <polinaoioi67@gmail.com>
2022-09-22 15:01:28 +0300

    Task68

---
+++ Seminar_9/Task68/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+//с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+
+int FunctionAckermann(int n, int m)
+{
+  if (n == 0) return m + 1;
+  else if (n != 0 && m == 0) return FunctionAckermann(n - 1, 1);
+  else return FunctionAckermann(n - 1,FunctionAckermann(n, m - 1));
+}
+
+Console.WriteLine(FunctionAckermann(2,3));
